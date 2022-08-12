707b19a6201dea0f42af8d2a18ac6bffc6b1b100
j23747 <josphatmwania237@gmail.com>
2022-08-12 18:34:00 +0300

    Initial commit

---
+++ src/main/java/com/SpringbootSecurewebexample/SimpleSecureSpringbootWebapp/SimpleSecureWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.SpringbootSecurewebexample.SimpleSecureSpringbootWebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSecureWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSecureWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringbootSecurewebexample/SimpleSecureSpringbootWebapp/SimpleSecureWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringbootSecurewebexample.SimpleSecureSpringbootWebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSecureWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
